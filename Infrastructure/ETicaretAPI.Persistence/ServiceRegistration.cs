1942e630ab0cf4908a4b7e5778ea419f8a1a245c
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2024-03-06 15:32:02 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        //Extension Metot
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
